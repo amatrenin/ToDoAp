718a6ab4a82cdd600d3876dc790199bed0f0414b
amatrenin <104056823+amatrenin@users.noreply.github.com>
2023-04-26 13:21:23 +0300

    add Unit tests

--- app/src/test/java/com/awddrhz/todoap/PrefRepositoryImplTest.kt
+++ app/src/test/java/com/awddrhz/todoap/PrefRepositoryImplTest.kt
@@ -3,24 +3,50 @@ package com.awddrhz.todoap
 import android.app.Application
 import android.content.SharedPreferences
 import com.awddrhz.todoap.data.sharedPrefs.PrefRepositoryImpl
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class PrefRepositoryImplTest {
     private lateinit var subject: PrefRepositoryImpl
-    private val application: SharedPreferences = mock()
+    private val sharedPreferencesMock: SharedPreferences = mock()
+    private val editMock: SharedPreferences.Editor = mock()
 
+    private val titleTest: String = "titleTest"
+    private val descriptionTest: String = "descriptionTest"
+    private val keyTest: String = "keyTest"
+    private val valueTest: String = "valueTest"
 
     @Before
     fun setup(){
-        subject = PrefRepositoryImpl(application)
+        subject = PrefRepositoryImpl(sharedPreferencesMock)
     }
 
     @Test
     fun getToDoItem_success(){
-        subject.getToDoItem()
+        `when`(sharedPreferencesMock.getString(
+            PrefRepositoryImpl.PREFS_TITLE_KEY,
+            PrefRepositoryImpl.PREFS_DEFAULT_VALUE
+        )
+        ).thenReturn(titleTest)
+        `when`(sharedPreferencesMock.getString(
+            PrefRepositoryImpl.PREFS_DESCRIPTION_KEY,
+            PrefRepositoryImpl.PREFS_DEFAULT_VALUE
+        )
+        ).thenReturn(descriptionTest)
+        val result = subject.getToDoItem()
+        assertEquals("titleTest", result.title)
+        assertEquals("descriptionTest", result.description)
+    }
+
+    @Test
+    fun saveDataInPrefs_success(){
+        `when`(sharedPreferencesMock.edit()).thenReturn(editMock)
+
+        subject.saveDataInPrefs(keyTest, valueTest)
+        verify(editMock).putString(keyTest, valueTest)
     }
 
 }
